f5d3c45d01f7dc4ff3ebde4f660410a75f17e92a
Ellis <ellis.ackerman@att.net>
2021-01-04 19:15:46 +0000

    changing architecture of gans

--- advgan_notebooks/advGAN_conv_cifar.py
+++ advgan_notebooks/advGAN_conv_cifar.py
@@ -9,66 +9,24 @@ import matplotlib.pyplot as plt
 import numpy as np
 from sklearn.metrics import accuracy_score
 
-# generator / discriminator set up
-# class Generator(nn.Module):
-#     def __init__(self, image_nc=1,ngf = 18):
-#         super(Generator, self).__init__()
-#         self.conv1 = nn.Conv2d(1, 6, 5, 1, 0)
-#         self.pool = nn.MaxPool2d(2, 2) 
-#         self.conv2 = nn.Conv2d(6, 16, 5)
-
-
-#         self.fc1 = nn.Linear(16 * 8 * 8, 120) 
-#         self.fc2 = nn.Linear(120, 84)  
-#         self.fc3 = nn.Linear(84,128)
-
-#         self.c3 = nn.ConvTranspose2d(128, ngf * 8, 4, 1, 0, bias=False)
-#         self.norm3 = nn.BatchNorm2d(ngf * 8)
-#         self.c2 = nn.ConvTranspose2d(ngf * 8, ngf * 4, 4, 2, 1, bias=False)
-#         self.norm2 = nn.BatchNorm2d(ngf * 4)
-#         self.c1 = nn.ConvTranspose2d( ngf * 4, ngf * 2, 4, 1, 0, bias=False)
-#         self.norm1 = nn.BatchNorm2d(ngf * 2)
-#         self.c0 = nn.ConvTranspose2d( ngf * 2, ngf, 4, 1, 0, bias=False)
-#         self.norm0 = nn.BatchNorm2d(ngf)
-#         self.c = nn.ConvTranspose2d( ngf, image_nc, 4, 2, 1, bias=False)
-
-#     def forward(self,x):
-#         leaky_relu = nn.LeakyReLU(.2)
-#         relu = nn.ReLU()
-#         tanh = nn.Tanh()
-#         # convolution part
-#         x = leaky_relu(self.pool(self.conv1(x)))
-#         x = leaky_relu(self.conv2(x))
-#         x = x.view(-1, 16 * 8 * 8)
-#         # linear part
-#         x = leaky_relu(self.fc1(x))
-#         x = leaky_relu(self.fc2(x))
-#         x = leaky_relu(self.fc3(x))
-#         x = x.reshape(x.shape[0],x.shape[1],1,1)
-#         # transpose conv part
-#         x = relu(self.norm3(self.c3(x)))
-#         x = relu(self.norm2(self.c2(x)))
-#         x = relu(self.norm1(self.c1(x)))
-#         x = relu(self.norm0(self.c0(x)))
-#         x = tanh(self.c(x))
-#         return x
-
 class Generator(nn.Module):
     def __init__(self, image_nc=1,ngf = 18):
         super(Generator, self).__init__()
-        self.conv1 = nn.Conv2d(1,1,9)
+        self.conv1 = nn.Conv2d(3,2,7)
         self.pool1 = nn.MaxPool2d(2,1) 
-        self.conv2 = nn.Conv2d(1,1,7)
-        self.pool2 = nn.MaxPool2d(2,1)
+        self.conv2 = nn.Conv2d(2,1,7)
+        self.pool2 = nn.MaxPool2d(2,1) 
 
-        self.fc1 = nn.Linear(144,100)
-        self.fc2 = nn.Linear(100,64) 
+        self.fc1 = nn.Linear(18*18,180) 
+        self.fc2 = nn.Linear(180,100)  
 
-        self.c2 = nn.ConvTranspose2d(1,1,7)
-        self.norm2 = nn.BatchNorm2d(1)
-        self.c1 = nn.ConvTranspose2d(1,1,11)
-        self.norm1 = nn.BatchNorm2d(1)
-        self.c0 = nn.ConvTranspose2d(1,1,5)
+        self.c3 = nn.ConvTranspose2d(1,2,7)
+        self.norm3 = nn.BatchNorm2d(2)
+        self.c2 = nn.ConvTranspose2d(2,3,7)
+        self.norm2 = nn.BatchNorm2d(3)
+        self.c1 = nn.ConvTranspose2d(3,3,7)
+        self.norm1 = nn.BatchNorm2d(3)
+        self.c0 = nn.ConvTranspose2d(3,3,5)
 
     def forward(self,x):
         leaky_relu = nn.LeakyReLU(.2)
@@ -81,8 +39,9 @@ class Generator(nn.Module):
         # linear part
         x = leaky_relu(self.fc1(x))
         x = leaky_relu(self.fc2(x))
-        x = x.reshape(x.shape[0],1,8,8)
+        x = x.reshape(x.shape[0],1,10,10)
         # transpose conv part
+        x = relu(self.norm3(self.c3(x)))
         x = relu(self.norm2(self.c2(x)))
         x = relu(self.norm1(self.c1(x)))
         x = tanh(self.c0(x))
@@ -91,16 +50,18 @@ class Generator(nn.Module):
 class Discriminator(nn.Module):
     def __init__(self, image_nc=1):
         super(Discriminator, self).__init__()
-        self.conv1 = nn.Conv2d(1,1,7)
-        self.pool1 = nn.MaxPool2d(1,1) 
-        self.conv2 = nn.Conv2d(1,1,7)
-        self.pool2 = nn.MaxPool2d(1,2)  
-        self.fc1 = nn.Linear(8*8,32) 
-        self.fc2 = nn.Linear(32,16) 
-        self.fc3 = nn.Linear(16,1) 
+        self.conv1 = nn.Conv2d(3,2,7)
+        self.pool1 = nn.MaxPool2d(2,1) 
+        self.conv2 = nn.Conv2d(2,1,7)
+        self.pool2 = nn.MaxPool2d(2,1) 
+        self.fc1 = nn.Linear(18*18,100) 
+        self.fc2 = nn.Linear(100,40) 
+        self.fc3 = nn.Linear(40,1) 
 
     def forward(self, x):
         leaky_relu = nn.LeakyReLU(.2)
+        relu = nn.ReLU()
+        tanh = nn.Tanh()
         x = leaky_relu(self.pool1(self.conv1(x)))
         x = leaky_relu(self.pool2(self.conv2(x)))
         x = x.reshape(x.shape[0],x.shape[-1]**2)
@@ -108,32 +69,10 @@ class Discriminator(nn.Module):
         x = leaky_relu(self.fc2(x))
         x = leaky_relu(self.fc3(x))
         return x
-
-# class Discriminator(nn.Module):
-#     def __init__(self, image_nc=1):
-#         super(Discriminator, self).__init__()
-#         # MNIST: 1*28*28
-#         model = [
-#             nn.Conv2d(image_nc, 8, kernel_size=4, stride=2, padding=0, bias=True),
-#             nn.LeakyReLU(0.2),
-#             # 8*13*13
-#             nn.Conv2d(8, 16, kernel_size=4, stride=2, padding=0, bias=True),
-#             nn.BatchNorm2d(16),
-#             nn.LeakyReLU(0.2),
-#             # 16*5*5
-#             nn.Conv2d(16, 32, kernel_size=4, stride=2, padding=0, bias=True),
-#             nn.BatchNorm2d(32),
-#             nn.LeakyReLU(0.2),
-#             nn.Conv2d(32, 1, 1),
-#             nn.Sigmoid()
-#             # 32*1*1
-#         ]
-#         self.model = nn.Sequential(*model)
-
-#     def forward(self, x):
-#         output = self.model(x).squeeze()
-#         return output
-
+    
+    
+    
+    
 # advgan class
 class advGAN():
 	"""
@@ -182,7 +121,7 @@ class advGAN():
 		self.c = c
 		self.gen_path = './advgan_models/'+gen_path_extra+'_convolutional_'+str(self.num_of_classes)+'classes_'+str(disc_coeff).replace('.','p')+'disc_'+str(hinge_coeff).replace('.','p')+'hinge_'+str(c).replace('.','p')+'c_'+str(adv_coeff).replace('.','p')+'adv.pt'
     
-	def show_tensor_images(self,image_tensor, num_images=25, size=(1, 28, 28)):
+	def show_tensor_images(self,image_tensor, num_images=25, size=(3, 32, 32)):
 		image_unflat = image_tensor.detach().cpu().view(-1, *size)
 		image_grid = make_grid(image_unflat[:num_images], nrow=5)
 		plt.imshow(image_grid.permute(1, 2, 0).squeeze())
---
+++ advgan_notebooks/net_conv_cifar.py
@@ -0,0 +1,116 @@
+import torch.nn as nn
+import torch.nn.functional as F
+import torch
+
+class BasicBlock(nn.Module):
+    expansion = 1
+
+    def __init__(self, in_planes, planes, stride=1):
+        super(BasicBlock, self).__init__()
+        self.conv1 = nn.Conv2d(
+            in_planes, planes, kernel_size=3, stride=stride, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(planes)
+        self.conv2 = nn.Conv2d(planes, planes, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn2 = nn.BatchNorm2d(planes)
+
+        self.shortcut = nn.Sequential()
+        if stride != 1 or in_planes != self.expansion*planes:
+            self.shortcut = nn.Sequential(
+                nn.Conv2d(in_planes, self.expansion*planes,
+                          kernel_size=1, stride=stride, bias=False),
+                nn.BatchNorm2d(self.expansion*planes)
+            )
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.bn2(self.conv2(out))
+        out += self.shortcut(x)
+        out = F.relu(out)
+        return out
+
+class target_net(nn.Module):
+    def __init__(self, block, num_blocks, num_classes=10):
+        super(target_net, self).__init__()
+        self.in_planes = 64
+
+        self.conv1 = nn.Conv2d(3, 64, kernel_size=3,
+                               stride=1, padding=1, bias=False)
+        self.bn1 = nn.BatchNorm2d(64)
+        self.layer1 = self._make_layer(block, 64, num_blocks[0], stride=1)
+        self.layer2 = self._make_layer(block, 128, num_blocks[1], stride=2)
+        self.layer3 = self._make_layer(block, 256, num_blocks[2], stride=2)
+        self.layer4 = self._make_layer(block, 512, num_blocks[3], stride=2)
+        self.linear = nn.Linear(512*block.expansion, num_classes)
+
+    def _make_layer(self, block, planes, num_blocks, stride):
+        strides = [stride] + [1]*(num_blocks-1)
+        layers = []
+        for stride in strides:
+            layers.append(block(self.in_planes, planes, stride))
+            self.in_planes = planes * block.expansion
+        return nn.Sequential(*layers)
+
+    def forward(self, x):
+        out = F.relu(self.bn1(self.conv1(x)))
+        out = self.layer1(out)
+        out = self.layer2(out)
+        out = self.layer3(out)
+        out = self.layer4(out)
+        out = F.avg_pool2d(out, 4)
+        out = out.view(out.size(0), -1)
+        out = self.linear(out)
+        return out
+    
+    def train(self, Data, criterion_tar, optimizer, device, n_epochs=25):
+        """
+        function to train the target net
+        Args - 
+            net: the target model
+            data: data to train on
+            criterion_tar: net criterion
+            optimizer: net optimizer
+            n_epochs: number of epochs to trian for
+            master_model: the model which we distill down to a net
+        Returns - 
+            net: trained net 
+        """
+        for epoch in range(n_epochs):  # loop over the dataset multiple times
+            running_loss = 0.0
+            for i, data in enumerate(Data, 0):
+                # get the inputs; data is a list of [inputs, labels]
+                inputs, labels = data
+                inputs = inputs.to(device)
+                labels = labels.to(device) 
+
+                # zero the parameter gradients
+                optimizer.zero_grad()
+
+                # forward + backward + optimize
+                outputs = self.forward(inputs).to(device)
+                loss = criterion_tar(outputs, labels)
+                loss.backward()
+                optimizer.step()
+            print('epoch/epochs: '+str(epoch+1)+'/'+str(n_epochs))
+            
+    def accuracy(self, Data, device): 
+        """
+        accuracy of the net
+        Args - 
+            net: the target model to test accuracy 
+            data: data to test accuracy
+        Resturns-
+            accuracy: correct/total * 100
+        """
+        correct = 0
+        total = 0
+        with torch.no_grad():
+            for data in Data:
+                images, labels = data
+                images = images.to(device)
+                labels = labels.to(device)
+                outputs = self.forward(images).to(device)
+                _, predicted = torch.max(outputs.data, 1)
+                total += labels.size(0)
+                correct += (predicted == labels).sum().item()
+        return 100.*correct/total
